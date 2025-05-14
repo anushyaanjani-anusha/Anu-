78a9e4641a0f287643b41f4574dd28161c48570d
anushyaanjani-anusha <anushaapsm@gmail.com>
2025-05-14 11:22:31 +0530

    Create shridevi.c

---
+++ shridevi.c
@@ -0,0 +1,13 @@
+#include <stdio.>
+int shridevi()
+{
+printf ("Anusha\n");
+printf ("date of birth\n");
+printf("age\n");
+printf("mobile number\n");
+}
+int main()
+{
+    shridevi ();
+    return 0;
+}
